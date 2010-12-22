668f2ee71dfeae30b0d8bf008275d23d64d2b87e
Nam Luu Nguyen <namluunguyen@gmail.com>
2010-12-21 18:26:24 -0800

    validation for forms added

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,6 +1,7 @@
 class Deal < ActiveRecord::Base
-	attr_accessor :title, :date, :price
-	
+	validates_presence_of :name, :description, :guid, :source, :buy_link, :cost, :cost_retail, :profit_margin
+	validates_numericality_of :cost, :cost_retail, :profit_margin
+  	
 	def to_s
 		return "#{@title} #{@description}"
 	end
