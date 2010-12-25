029b8a1d6c580b2ffe975b96fb25d4375443b13b
Nam Luu Nguyen <namluunguyen@gmail.com>
2010-12-25 04:39:14 -0800

    stash flip categories added on front page

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -6,4 +6,14 @@ class WebsiteController < ApplicationController
 
   def about
   end
+  
+  def stashindex
+  	page = params[:page] || 1
+	@deals = Deal.paginate(:page=>page, :per_page=>20, :order => 'publish_date DESC', :conditions => ['stashflip_status = ?', 'stash'])  	
+  end
+  
+  def flipindex
+  	page = params[:page] || 1
+	@deals = Deal.paginate(:page=>page, :per_page=>20, :order => 'publish_date DESC', :conditions => ['stashflip_status = ?', 'flip'])  	
+  end
 end
