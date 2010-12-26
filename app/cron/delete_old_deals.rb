e6029b4cce698914c2eaa0dca98246602e432ef0
Nam Luu Nguyen <namluunguyen@gmail.com>
2010-12-26 15:19:04 -0800

    cron

--- app/cron/delete_old_deals.rb
+++ app/cron/delete_old_deals.rb
@@ -1,7 +1,7 @@
 class DeleteOldDeals < ActiveRecord::Base
   # This script deletes all posts that are over 5 minutes old
 
-    deal_ids = Deal.find(:all, :conditions => ["created_at < ? and permadeal = ?", 2.minutes.ago, 'no'])
+    deal_ids = Deal.find(:all, :conditions => ["created_at < ? and permadeal = ?", 7.days.ago, 'no'])
 
   if deal_ids.size > 0
     Deal.destroy(deal_ids)
