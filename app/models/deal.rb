18c476c5c3a8f368c0d523e5431de2b4b9c26456
Nam Luu Nguyen <namluunguyen@gmail.com>
2010-12-27 19:00:09 -0800

    resizing implemented

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -2,7 +2,8 @@ class Deal < ActiveRecord::Base
 	validates_presence_of :name, :description, :guid, :source, :buy_link, :cost, :cost_retail, :profit_margin, :stashflip_status, :permadeal
 	validates_numericality_of :cost, :cost_retail, :profit_margin
 	
-	has_attached_file :photo,	:url  => "/assets/deals/:id/:style/:basename.:extension",
+	has_attached_file :photo,	:styles => { :small => "130x130>" },
+								:url  => "/assets/deals/:id/:style/:basename.:extension",
                   				:path => ":rails_root/public/assets/deals/:id/:style/:basename.:extension"
  	
 	def to_s
