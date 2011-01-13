b00acae52e9c673d766442246741c842e7311eb9
Nam Luu Nguyen <namluunguyen@gmail.com>
2011-01-13 01:55:53 -0800

    cleaned up view

--- public/javascripts/application.js
+++
@@ -1,8 +0,0 @@
-function openGoogleDirections(dest_address) {
- 
-  var my_addr = document.getElementById('input_address').value;
-  var general_url = 'http://maps.google.com/maps?saddr='+my_addr +'&daddr=' + dest_address;
-  window.open(general_url, 'Beatmap => Google Maps Directions', 'width=1000, height=500, toolbar=yes, location=yes, directories=yes, status=yes, menubar=yes, scrollbars=yes, copyhistory=yes, resizable=yes');
-
-/*   alert('Hello');*/
-}
