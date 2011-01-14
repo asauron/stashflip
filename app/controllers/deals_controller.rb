f095dca10eb970293c34601bb3a6c8e57b651710
Nam Luu Nguyen <namluunguyen@gmail.com>
2011-01-14 04:02:13 -0800

    status fixed

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -84,10 +84,10 @@ class DealsController < ApplicationController
   
   def post_deal 	
 	  post_deal_1
-	  #post_deal_2
-	  #post_deal_3
-	  #post_deal_4
-	  #post_deal_5	  	  	  	  
+	  post_deal_2
+	  post_deal_3
+	  post_deal_4
+	  post_deal_5	  	  	  	  
   	      	  
 	  redirect_to :root, :notice => "Deals have been refreshed!"    
   end
